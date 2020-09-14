e19135904a0dbe7b4ededdee4e9e1cd825599c2b
unknown <hieusmiths@gmail.com>
2020-09-14 20:41:47 +0700

    Test Protected Component

--- src/component/protect-route/__test__/procted_route.test.tsx
+++ src/component/protect-route/__test__/procted_route.test.tsx
@@ -1,13 +1,52 @@
-import { mount } from 'enzyme';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { MemoryRouter, Route } from 'react-router-dom';
+import configStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import ProtectedRoute from '../ProtectedRoute';
+import TodoPage from '../../../ToDoPage';
+
+const mockStore = configStore();
 
 describe('<ProtectedRoute />', () => {
   it('Should render corectly', () => {
-    const wrapper = mount(<ProtectedRoute />);
+    const store = mockStore({ auth: {} });
+    const wrapper = shallow(<Provider store={store}><ProtectedRoute /></Provider>);
     expect(toJson(wrapper)).toMatchSnapshot();
-  })
-  it('Should <Redirect /> when unlogin', () => {
+  });
 
+  it('Should <Redirect /> when unlogin', () => {
+    const initState = {
+      auth: {
+        isLogin: false,
+      }
+    };
+    const store = mockStore(initState);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ProtectedRoute />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find(TodoPage)).toHaveLength(0);
+  });
+
+  it('Should render Route when login', () => {
+    const initState = {
+      auth: {
+        isLogin: true,
+      }
+    };
+    const store = mockStore(initState);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ProtectedRoute path='/todo' component={TodoPage} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find(TodoPage)).toHaveLength(1);
   });
 });
