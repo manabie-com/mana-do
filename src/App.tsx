a38cb31697715d91f6796efcf537dae45f8e67d1
Phạm Đức Bình <binh1298@gmail.com>
2021-01-13 17:18:02 +0700

    Init folder structure for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
-import SignInPage from './SignInPage';
-import ToDoPage from './ToDoPage';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import SignInPage from './pages/SignIn';
+import ToDoPage from './pages/ToDo';
 
 function App() {
   return (
     <main className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={SignInPage}/>
-          <Route path="/todo" component={ToDoPage}/>
+          <Route path="/" exact component={SignInPage} />
+          <Route path="/todo" component={ToDoPage} />
         </Switch>
       </BrowserRouter>
     </main>
