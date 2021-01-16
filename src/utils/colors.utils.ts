6aaffd6b9f06ad46005fd5dc867713a24794455c
Phạm Đức Bình <binh1298@gmail.com>
2021-01-17 01:24:49 +0700

    Implement spacings & color utils

---
+++ src/utils/colors.utils.ts
@@ -0,0 +1,13 @@
+// This color pallete follow one of the material studies: Basil
+// https://material.io/design/material-studies/basil.html#color
+export const colorPalete = {
+  primary: '#356859',
+  primaryFirstVariant: '#37966F',
+  primarySecondVariant: '#B9E4C9',
+  secondary: '#FD5523',
+  background: '#FFFBE6',
+  surface: '#FFFBE6',
+  error: '#B00020',
+  onSurface: '#356859',
+  onSurfaceSecondary: '#FD5523'
+}
--- src/utils/spacing.utils.ts
+++ src/utils/spacing.utils.ts
@@ -3,3 +3,36 @@ import { LayoutSpacing, SCALING_FACTOR } from "../constants/layouts.constants";
 export const calcSpacing = (spacing: LayoutSpacing) => {
   return spacing ? `${spacing * SCALING_FACTOR}px` : undefined;
 }
+
+export type BreakPoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type BreakPointMaxWidth = 'sm' | 'md' | 'lg' | 'xl';
+export const calcBreakPoint = (breakpoint: BreakPoint) => {
+  switch (breakpoint) {
+    case 'xs':
+      return '0px';
+    case 'sm':
+      return '600px';
+    case 'md':
+      return '960px';
+    case 'lg':
+      return '1280px';
+    case 'xl':
+      return '1920px';
+    default:
+      return '960px';
+  }
+}
+
+export type FontSize = 'small' | 'medium' | 'big' | undefined
+export const calcFontSize = (fontSize: FontSize) => {
+  switch (fontSize) {
+    case 'small':
+      return '1rem';
+    case 'medium':
+      return '1.2rem';
+    case 'big':
+      return '2rem';
+    default:
+      return '1rem';
+  }
+}
