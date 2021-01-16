be344fefb9811d5033926c79ffc1ed4b19bb1148
Phạm Đức Bình <binh1298@gmail.com>
2021-01-17 02:22:16 +0700

    Fix hoc name

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import withPageName from '../hoc/withBackgroundColor';
+import withPageName from '../hoc/withPageName';
 import localStorageUtils from '../utils/localStorage.utils';
 
 export const PublicRoute: React.FC<any> = ({ component, name, ...rest }) => {
