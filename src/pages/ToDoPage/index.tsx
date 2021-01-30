40d45cad18977a2e66ddde410e2542cadfb05b89
txd22081999 <txd22081999@gmail.com>
2021-01-30 19:25:39 +0700

    split to component & edit todo function

--- src/pages/ToDoPage/index.tsx
+++ src/pages/ToDoPage/index.tsx
@@ -1,135 +1,166 @@
-import React, {useEffect, useReducer, useRef, useState} from 'react';
-import {RouteComponentProps} from 'react-router-dom';
+import React, { useEffect, useReducer, useRef, useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
-import reducer, {initialState} from '../../store/reducer';
+import reducer, { initialState } from '../../store/reducer';
 import {
-    setTodos,
-    createTodo,
-    deleteTodo,
-    toggleAllTodos,
-    deleteAllTodos,
-    updateTodoStatus
+  setTodos,
+  createTodo,
+  deleteTodo,
+  toggleAllTodos,
+  deleteAllTodos,
+  updateTodoStatus,
+  updateTodo
 } from '../../store/actions';
 import Service from '../../service';
-import {TodoStatus} from '../../models/todo';
-import {isTodoCompleted} from '../../utils';
+import { TodoStatus } from '../../models/todo';
+import { isTodoCompleted } from '../../utils';
 
-import './index.css'
+import './index.css';
+import TodoItem from '../../components/TodoItem';
 
 type EnhanceTodoStatus = TodoStatus | 'ALL';
 
+const ToDoPage = ({ history }: RouteComponentProps) => {
+  const [{ todos }, dispatch] = useReducer(reducer, initialState);
+  const [todoInput, setTodoInput] = useState('');
+  const [showing, setShowing] = useState<EnhanceTodoStatus>('ALL');
+  const [editId, setEditId] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
 
-const ToDoPage = ({history}: RouteComponentProps) => {
-    const [{todos}, dispatch] = useReducer(reducer, initialState);
-    const [showing, setShowing] = useState<EnhanceTodoStatus>('ALL');
-    const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    (async () => {
+      const resp = await Service.getTodos();
 
-    useEffect(()=>{
-        (async ()=>{
-            const resp = await Service.getTodos();
+      console.log('RUN', resp);
+      dispatch(setTodos(resp || []));
+    })();
+  }, []);
 
-            dispatch(setTodos(resp || []));
-        })()
-    }, [])
-
-    const onCreateTodo = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter' && inputRef.current) {
-            try {
-                const resp = await Service.createTodo(inputRef.current.value);
-                dispatch(createTodo(resp));
-                inputRef.current.value = '';
-            } catch (e) {
-                if (e.response.status === 401) {
-                    history.push('/')
-                }
-            }
+  const onCreateTodo = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const inputValue = todoInput.trim();
+    if (e.key === 'Enter' && inputValue.length > 0) {
+      try {
+        const resp = await Service.createTodo(inputValue);
+        dispatch(createTodo(resp));
+        // Clear todo input
+        setTodoInput('');
+      } catch (e) {
+        if (e.response.status === 401) {
+          history.push('/');
         }
+      }
     }
+  };
 
-    const onUpdateTodoStatus = (e: React.ChangeEvent<HTMLInputElement>, todoId: string) => {
-        dispatch(updateTodoStatus(todoId, e.target.checked))
-    }
+  const onToggleAllTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(toggleAllTodos(e.target.checked));
+  };
 
-    const onToggleAllTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(toggleAllTodos(e.target.checked))
-    }
+  const onDeleteAllTodo = () => {
+    dispatch(deleteAllTodos());
+  };
 
-    const onDeleteAllTodo = () => {
-        dispatch(deleteAllTodos());
-    }
+  const showTodos = todos
+    .filter((todo) => {
+      switch (showing) {
+        case TodoStatus.ACTIVE:
+          return todo.status === TodoStatus.ACTIVE;
+        case TodoStatus.COMPLETED:
+          return todo.status === TodoStatus.COMPLETED;
+        default:
+          return true;
+      }
+    })
+    .sort((a, b) => (a.created_date > b.created_date ? -1 : 1)); // sort by recent time order
 
-    const showTodos = todos.filter((todo) => {
-        switch (showing) {
-            case TodoStatus.ACTIVE:
-                return todo.status === TodoStatus.ACTIVE;
-            case TodoStatus.COMPLETED:
-                return todo.status === TodoStatus.COMPLETED;
-            default:
-                return true;
-        }
-    });
+  const activeTodos = todos.reduce(function (accum, todo) {
+    return isTodoCompleted(todo) ? accum : accum + 1;
+  }, 0);
 
-    const activeTodos = todos.reduce(function (accum, todo) {
-        return isTodoCompleted(todo) ? accum : accum + 1;
-    }, 0);
+  const onTodoInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoInput(e.currentTarget.value);
+  };
 
-    return (
-        <div className="ToDo__container">
-            <div className="Todo__creation">
-                <input
-                    ref={inputRef}
-                    className="Todo__input"
-                    placeholder="What need to be done?"
-                    onKeyDown={onCreateTodo}
-                />
-            </div>
-            <div className="ToDo__list">
-                {
-                    showTodos.map((todo, index) => {
-                        return (
-                            <div key={index} className="ToDo__item">
-                                <input
-                                    type="checkbox"
-                                    checked={isTodoCompleted(todo)}
-                                    onChange={(e) => onUpdateTodoStatus(e, todo.id)}
-                                />
-                                <span>{todo.content}</span>
-                                <button
-                                    className="Todo__delete"
-                                    onClick={() => dispatch(deleteTodo(todo.id))}
-                                >
-                                    X
-                                </button>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-            <div className="Todo__toolbar">
-                {todos.length > 0 ?
-                    <input
-                        type="checkbox"
-                        checked={activeTodos === 0}
-                        onChange={onToggleAllTodo}
-                    /> : <div/>
-                }
-                <div className="Todo__tabs">
-                    <button className="Action__btn" onClick={()=>setShowing('ALL')}>
-                        All
-                    </button>
-                    <button className="Action__btn" onClick={()=>setShowing(TodoStatus.ACTIVE)}>
-                        Active
-                    </button>
-                    <button className="Action__btn" onClick={()=>setShowing(TodoStatus.COMPLETED)}>
-                        Completed
-                    </button>
-                </div>
-                <button className="Action__btn" onClick={onDeleteAllTodo}>
-                    Clear all todos
-                </button>
-            </div>
+  const handleEdit = (id: string) => {
+    // setTodoInput(e.currentTarget.value);
+    console.log(id);
+    setEditId(id);
+    //    dispatch(updateTodo(id, ))
+  };
+
+  return (
+    <div className='ToDo__container'>
+      <div className='Todo__creation'>
+        <input
+          ref={inputRef}
+          value={todoInput}
+          className='Todo__input'
+          placeholder='What need to be done?'
+          onKeyDown={onCreateTodo}
+          onChange={onTodoInput}
+        />
+      </div>
+      <div className='ToDo__list'>
+        {showTodos.map((todo) => {
+          return <TodoItem todo={todo} />;
+          //   const { id = '123', content = '' } = todo;
+          //   return (
+          //     <div key={id} className='ToDo__item'>
+          //       <input
+          //         type='checkbox'
+          //         checked={isTodoCompleted(todo)}
+          //         onChange={(e) => onUpdateTodoStatus(e, id)}
+          //       />
+
+          //       <input className='Todo__content' value={content} disabled></input>
+
+          //       <div
+          //         className='Todo__delete'
+          //         onClick={() => dispatch(deleteTodo(id))}
+          //       >
+          //         <i className='bx bx-x icon'></i>
+          //       </div>
+
+          //       <div className='Todo__edit' onClick={() => handleEdit(id)}>
+          //         <i className='bx bxs-pencil icon'></i>
+          //       </div>
+          //     </div>
+          //   );
+        })}
+      </div>
+      <div className='Todo__toolbar'>
+        {todos.length > 0 ? (
+          <input
+            type='checkbox'
+            checked={activeTodos === 0}
+            onChange={onToggleAllTodo}
+          />
+        ) : (
+          <div />
+        )}
+        <div className='Todo__tabs'>
+          <button className='Action__btn' onClick={() => setShowing('ALL')}>
+            All
+          </button>
+          <button
+            className='Action__btn'
+            onClick={() => setShowing(TodoStatus.ACTIVE)}
+          >
+            Active
+          </button>
+          <button
+            className='Action__btn'
+            onClick={() => setShowing(TodoStatus.COMPLETED)}
+          >
+            Completed
+          </button>
         </div>
-    );
+        <button className='Action__btn' onClick={onDeleteAllTodo}>
+          Clear all todos
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ToDoPage;
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,64 +1,109 @@
-import {Todo, TodoStatus} from '../models/todo';
+import { Todo, TodoStatus } from '../models/todo';
 import {
   AppActions,
   CREATE_TODO,
   DELETE_ALL_TODOS,
   DELETE_TODO,
+  UPDATE_TODO,
   TOGGLE_ALL_TODOS,
   UPDATE_TODO_STATUS
 } from './actions';
 
 export interface AppState {
-  todos: Array<Todo>
+  todos: Array<Todo>;
 }
 
 export const initialState: AppState = {
   todos: []
-}
+};
 
 function reducer(state: AppState, action: AppActions): AppState {
-  switch (action.type) {
-    case CREATE_TODO:
-      state.todos.push(action.payload);
-      return {
-        ...state
-      };
+  const { todos = [] } = state;
+  console.log(state);
 
-    case UPDATE_TODO_STATUS:
-      const index2 = state.todos.findIndex((todo) => todo.id === action.payload.todoId);
-      state.todos[index2].status = action.payload.checked ? TodoStatus.COMPLETED : TodoStatus.ACTIVE;
+  switch (action.type) {
+    case CREATE_TODO: {
+      // console.log(action.payload);
+      const newTodo: Todo = action.payload;
 
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: [...state.todos, newTodo]
+      };
+    }
+
+    case UPDATE_TODO_STATUS:
+      const index2 = state.todos.findIndex(
+        (todo) => todo.id === action.payload.todoId
+      );
+      console.log(state.todos);
+      console.log(action.payload.todoId);
+      // state.todos[index2].status = action.payload.checked
+      //   ? TodoStatus.COMPLETED
+      //   : TodoStatus.ACTIVE;
+
+      return {
+        ...state
+        // todos: state.todos
+      };
 
     case TOGGLE_ALL_TODOS:
-      const tempTodos = state.todos.map((e)=>{
+      const tempTodos = state.todos.map((e) => {
         return {
           ...e,
           status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE
-        }
-      })
+        };
+      });
 
       return {
         ...state,
         todos: tempTodos
-      }
+      };
 
     case DELETE_TODO:
-      const index1 = state.todos.findIndex((todo) => todo.id === action.payload);
+      const index1 = state.todos.findIndex(
+        (todo) => todo.id === action.payload
+      );
       state.todos.splice(index1, 1);
 
       return {
         ...state,
         todos: state.todos
-      }
+      };
+
     case DELETE_ALL_TODOS:
       return {
         ...state,
         todos: []
+      };
+
+    case UPDATE_TODO: {
+      const {
+        payload: { todoId, content = '' }
+      } = action;
+      const { todos } = state;
+      const currentIndex = todos.findIndex((todo) => todo.id === todoId);
+
+      if (currentIndex === -1) {
+        console.log(`Todo item's index not found`);
+        return {
+          ...state
+        };
       }
+
+      const todoList = todos;
+      const updatedTodo = todos[currentIndex];
+      updatedTodo.content = content;
+      // state.todos.splice(index1, 1);
+      todoList.splice(currentIndex, 1, updatedTodo);
+      console.log(todos);
+      console.log(todoList);
+
+      return {
+        ...state,
+        todos: todoList
+      };
+    }
     default:
       return state;
   }
