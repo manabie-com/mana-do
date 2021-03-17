7b973364094ba0e1442f4171e172e1390eeb6c17
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-03-17 18:55:36 +0700

    Fix todo manipulation bugs/anti-patterns

--- src/store/ManaDo_todo/reducer.ts
+++ src/store/ManaDo_todo/reducer.ts
@@ -12,22 +12,41 @@ import { ITodoType } from "./types";
 function reducer(state: ITodoType, action: AppActions): ITodoType {
   switch (action.type) {
     case CREATE_TODO:
-      state.todos.push(action.payload);
+      // state.todos.push(action.payload); // Anti pattern, state should not be mutable
+      // return {
+      //   ...state,
+      // };
+
       return {
         ...state,
+        todos: [...state.todos, action.payload], // treat state as immutable
       };
 
     case UPDATE_TODO_STATUS:
-      const index2 = state.todos.findIndex(
-        (todo) => todo.id === action.payload.todoId
-      );
-      state.todos[index2].status = action.payload.checked
-        ? TodoStatus.COMPLETED
-        : TodoStatus.ACTIVE;
+      // const index2 = state.todos.findIndex(
+      //   (todo) => todo.id === action.payload.todoId
+      // );
+      // state.todos[index2].status = action.payload.checked // Anti pattern, state should not be mutable
+      //   ? TodoStatus.COMPLETED
+      //   : TodoStatus.ACTIVE;
+
+      // return {
+      //   ...state,
+      //   todos: state.todos,
+      // };
 
       return {
         ...state,
-        todos: state.todos,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.todoId
+            ? {
+                ...todo,
+                status: action.payload.checked
+                  ? TodoStatus.COMPLETED
+                  : TodoStatus.ACTIVE,
+              }
+            : todo
+        ),
       };
 
     case TOGGLE_ALL_TODOS:
@@ -44,20 +63,27 @@ function reducer(state: ITodoType, action: AppActions): ITodoType {
       };
 
     case DELETE_TODO:
-      const index1 = state.todos.findIndex(
-        (todo) => todo.id === action.payload
-      );
-      state.todos.splice(index1, 1);
+      // const index1 = state.todos.findIndex(
+      //   (todo) => todo.id === action.payload
+      // );
+      // state.todos.splice(index1, 1); // Anti pattern, state should not be mutable
+
+      // return {
+      //   ...state,
+      //   todos: state.todos,
+      // };
 
       return {
         ...state,
-        todos: state.todos,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
       };
+
     case DELETE_ALL_TODOS:
       return {
         ...state,
         todos: [],
       };
+
     default:
       return state;
   }
