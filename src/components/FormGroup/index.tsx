e35e93d3c8edb1d29d876b7780fa7c93e51049a4
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-03-17 18:28:25 +0700

    Add FormGroup component for Mana-Do

---
+++ src/components/FormGroup/index.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import styles from "./FormGroup.module.css";
+
+const FormGroup = ({
+  name = "",
+  id = "",
+  label = "",
+  value = "",
+  type = "text",
+  placeholder = "Place holder",
+  feedbackLabel = "",
+  required = false,
+  onChange = (e: React.ChangeEvent<HTMLInputElement>) => {},
+  ...props
+}) => {
+  return (
+    <div className={`${styles.ManaDo__formGroup} ${props.className || ""}`}>
+      <label className={styles.ManaDo__formGroupLabel} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className={`${styles.ManaDo__formGroupInput} ${
+          feedbackLabel && styles.ManaDo__inputError
+        }`}
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        required={required}
+        placeholder={placeholder}
+        onChange={onChange}
+        autoComplete="on"
+      />
+      <span className={styles.ManaDo__formGroupFeedbackLabel}>
+        {feedbackLabel}
+      </span>
+    </div>
+  );
+};
+
+export default React.memo(FormGroup);
