e68f1b64451e7ba70644a390c56bdacb83adaf8d
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-03-17 18:30:07 +0700

    Refactor SignInPage view

--- src/views/SignInPage/index.tsx
+++ src/views/SignInPage/index.tsx
@@ -1,66 +1,47 @@
-import React, { useState } from "react";
-import "./SignInPage.module.css";
+import React from "react";
+import styles from "./SignInPage.module.css";
 
-import { useHistory } from "react-router-dom";
-import { AUTH_TOKEN } from "../../constants";
-import Service from "../../service";
-import useLocalStorage from "../../_hooks/useLocalStorage";
+import LoginForm from "../../components/LoginForm";
+import ManaDoContainer from "../../components/ManaDoContainer";
 
 const SignInPage = () => {
-  const [, setToken] = useLocalStorage(AUTH_TOKEN);
-  const [form, setForm] = useState({
-    userId: "",
-    password: "",
-  });
-  const history = useHistory();
-
-  const signIn = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const resp = await Service.signIn(form.userId, form.password);
-
-    setToken(resp);
-    history.push("/todo");
-  };
-
-  const onChangeField = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.persist();
-    setForm((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
   return (
-    <div style={{ marginTop: "3rem", textAlign: "left" }}>
-      <form onSubmit={signIn}>
-        <label htmlFor="user_id">
-          User id
-          <input
-            id="user_id"
-            name="userId"
-            value={form.userId}
-            style={{ marginTop: 12 }}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor="password">
-          Password
-          <input
-            id="password"
-            name="password"
-            type="password"
-            style={{ marginTop: 12 }}
-            value={form.password}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <button type="submit" style={{ marginTop: 12 }}>
-          Sign in
-        </button>
-      </form>
-    </div>
+    // <div style={{ marginTop: "3rem", textAlign: "left" }}>
+    //   <form onSubmit={signIn}>
+    //     <label htmlFor="user_id">
+    //       User id
+    //       <input
+    //         id="user_id"
+    //         name="userId"
+    //         value={form.userId}
+    //         style={{ marginTop: 12 }}
+    //         onChange={onChangeField}
+    //       />
+    //     </label>
+    //     <br />
+    //     <label htmlFor="password">
+    //       Password
+    //       <input
+    //         id="password"
+    //         name="password"
+    //         type="password"
+    //         style={{ marginTop: 12 }}
+    //         value={form.password}
+    //         onChange={onChangeField}
+    //       />
+    //     </label>
+    //     <br />
+    //     <button type="submit" style={{ marginTop: 12 }}>
+    //       Sign in
+    //     </button>
+    //   </form>
+    // </div>
+    <ManaDoContainer className={styles.ManaDo__SignInPageContainer}>
+      <div className={styles.ManaDo__LoginFormWrapper}>
+        <h1 className={styles.ManaDo__LogoName}>Mana-do</h1>
+        <LoginForm className="mt-1" />
+      </div>
+    </ManaDoContainer>
   );
 };
 
