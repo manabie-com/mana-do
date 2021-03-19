d2caf629f191136db9aec6ca941bdf08cd1b18d2
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-03-20 00:27:01 +0700

    Animate waving~~~ header

--- src/views/SignInPage/index.tsx
+++ src/views/SignInPage/index.tsx
@@ -6,36 +6,6 @@ import ManaDoContainer from "../../components/ManaDoContainer";
 
 const SignInPage = () => {
   return (
-    // <div style={{ marginTop: "3rem", textAlign: "left" }}>
-    //   <form onSubmit={signIn}>
-    //     <label htmlFor="user_id">
-    //       User id
-    //       <input
-    //         id="user_id"
-    //         name="userId"
-    //         value={form.userId}
-    //         style={{ marginTop: 12 }}
-    //         onChange={onChangeField}
-    //       />
-    //     </label>
-    //     <br />
-    //     <label htmlFor="password">
-    //       Password
-    //       <input
-    //         id="password"
-    //         name="password"
-    //         type="password"
-    //         style={{ marginTop: 12 }}
-    //         value={form.password}
-    //         onChange={onChangeField}
-    //       />
-    //     </label>
-    //     <br />
-    //     <button type="submit" style={{ marginTop: 12 }}>
-    //       Sign in
-    //     </button>
-    //   </form>
-    // </div>
     <ManaDoContainer className={styles.ManaDo__SignInPageContainer}>
       <div className={styles.ManaDo__LoginFormWrapper}>
         <h1 className={styles.ManaDo__LogoName}>Mana-do</h1>
--- src/views/TodoPage/index.tsx
+++ src/views/TodoPage/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import "./TodoPage.module.css";
+import styles from "./TodoPage.module.css";
 
 import ManaDoContainer from "../../components/ManaDoContainer";
 import Header from "../../components/Header";
 import TodoFeatureSection from "../../components/TodoFeatureSection";
 import Footer from "../../components/Footer";
+import { ReactComponent as Wave } from "../../svgs/wave.svg";
 
 const ToDoPage = () => {
   return (
     <>
+      <Wave className={styles.ManaDo__AnimateWave} />
       <ManaDoContainer>
         <Header />
         <TodoFeatureSection />
