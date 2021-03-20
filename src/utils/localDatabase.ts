38e5803848b8fa4205be0d83ddfdd1f0d20d598b
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-03-21 02:56:23 +0700

    Provide comments for utils

--- src/utils/localDatabase.ts
+++ src/utils/localDatabase.ts
@@ -1,6 +1,7 @@
 import { Todo } from "../models/todo";
 import { User } from "../models/user";
 
+// FullUser Schema/Interface
 export interface FullUser extends User {
   user_id: string;
   username: string;
@@ -13,8 +14,11 @@ export interface IManaDo_DB {
   users: Array<FullUser>;
 }
 
+// Simple LocalStorage Database (for the purpose of this front-end project only!)
+// You shouldn't store large datas in local storage anyway.
 export const ManaDo_DB: IManaDo_DB = {
   todos: [],
+  // Preseted user for the application
   users: [
     {
       user_id: "firstUser",
