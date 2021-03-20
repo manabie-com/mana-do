82a926c4af05e83a83815f642ba9a891a0d74f41
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-03-21 00:55:06 +0700

    Provide comments for authorization pending page

--- src/auth/LoginPendingPage/index.tsx
+++ src/auth/LoginPendingPage/index.tsx
@@ -1,52 +1,72 @@
 import * as React from "react";
+
+import styles from "./LoginPendingPage.module.css";
 import { useHistory, useLocation } from "react-router";
-import Loading from "../../components/Loading";
+import { UserContext } from "../../store/contexts/userContext";
+import { setUser } from "../../store/actions/userActions";
 import { AUTH_TOKEN } from "../../constants";
 import Service from "../../service";
-import { setUser } from "../../store/actions/userActions";
-import { UserContext } from "../../store/contexts/userContext";
-import styles from "./LoginPendingPage.module.css";
+
+import Loading from "../../components/Loading";
 
 export interface LoginPendingPageProps {}
 
+// This component will be the bridge of login authorization
+// returns a loading view as user will be waiting.
+// SetTimeout functions for kinda-real API call experience
 const LoginPendingPage: React.FunctionComponent<LoginPendingPageProps> = () => {
-  const [, dispatch] = React.useContext(UserContext);
   const history = useHistory();
   const location = useLocation();
-  const [message, setMessage] = React.useState("Checking...");
+  const [, dispatch] = React.useContext(UserContext);
+  const [message, setMessage] = React.useState("Checking");
+
+  const errorHandler = React.useCallback(
+    (error?) => {
+      setMessage("There is something wrong...");
+      setTimeout(() => {
+        history.push(
+          "/",
+          error || "There is something wrong with the server..."
+        );
+      }, 1000);
+    },
+    [history]
+  );
 
   React.useEffect(() => {
-    const token = location.state as string;
-    console.log(token);
-
-    setTimeout(async () => {
-      // Checking...
-      if (token) {
+    const token = (location.state as string) || "";
+    // Check route param
+    if (token) {
+      (async () => {
         try {
           const user = await Service.getUser(token);
-          setMessage("Welcome");
-          setTimeout(() => {
-            // Welcome
-            localStorage.setItem(AUTH_TOKEN, token);
-            dispatch(setUser(user));
-
-            history.push("/todo");
-          }, 2000);
+          // Wait for user fetch by token
+          setTimeout(async () => {
+            if (user) {
+              // Display "Welcome" message as user is authorized
+              setMessage("Welcome");
+              localStorage.setItem(AUTH_TOKEN, token);
+              dispatch(setUser(user));
+              // Wait 1.5s for "adventure experience"?
+              setTimeout(() => {
+                // Then redirect user to TodoPage
+                history.push("/todo");
+              }, 2000);
+            } else {
+              // User is not authorized/ user not found,...
+              errorHandler();
+            }
+          }, 1000);
         } catch (error) {
-          setMessage("There is something wrong...");
-          setTimeout(() => {
-            // There is something wrong...
-            history.push("/");
-          }, 2000);
+          // API error catch then redirect user to login page
+          errorHandler(error);
         }
-      } else {
-        setMessage("There is something wrong...");
-        setTimeout(() => {
-          history.push("/");
-        }, 1000);
-      }
-    }, 1000);
-  }, [dispatch, history, location.state]);
+      })();
+    } else {
+      // Token is not valid
+      errorHandler();
+    }
+  }, [dispatch, errorHandler, history, location.state]);
 
   return (
     <div className={styles.ManaDo__LoginPendingPage}>
