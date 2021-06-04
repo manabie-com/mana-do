ab780efa43b0895dadb6a908a32e69d8dc55e9eb
DESKTOP-OS3KJN9\nghoa <nghoang2013@gmail.com>
2021-06-05 02:50:53 +0700

    [add] globalTheme

---
+++ src/globalTheme.ts
@@ -0,0 +1,17 @@
+const globalTheme = {
+  color: {
+    primary: '#FFAE42',
+    secondary: '',
+  },
+  fontSize: {
+    default: '16px',
+  },
+  size: {
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+  },
+};
+
+export default globalTheme;
