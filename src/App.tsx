1f57d900b6097f1c4da8324862776064d7747e27
xtreme <nghoang2013@gmail.com>
2021-06-04 16:03:12 +0700

    [restructure] files

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import SignInPage from './SignInPage';
-import ToDoPage from './ToDoPage';
+import SignInPage from './pages/SignInPage';
+import ToDoPage from './pages/ToDoPage';
 
 import './App.css';
+import Spinner from './components/Loading/LoadingSpinner';
 
 function App() {
   return (
     <main className="App">
+      <Spinner />
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={SignInPage}/>
---
+++ src/components/Loading/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.scss';
+
+function Spinner() {
+	return (
+		<div className="loader">Loading...</div>
+	)
+}
+
+export default Spinner
