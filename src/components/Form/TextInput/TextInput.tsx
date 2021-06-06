66ad259596a9f45af8daf7b60d2204d8c311a1f7
DESKTOP-OS3KJN9\nghoa <nghoang2013@gmail.com>
2021-06-06 17:13:45 +0700

    [update] add index file for TextInput

---
+++ src/components/Form/TextInput/TextInput.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import theme from 'globalTheme';
+import { InputType } from './types';
+
+const StyledFormControl = styled.div<InputType>`
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+    `}
+`;
+
+const StyledLabel = styled.label`
+  display: inline-block;
+  width: 30%;
+  text-align: right;
+  padding-right: 3%;
+`;
+
+const StyledInput = styled.input<InputType>`
+  outline: none;
+  border: none;
+  border-bottom: 1px solid
+    ${(props) => (props.error ? theme.color.error : theme.color.gray)};
+  background: none;
+  color: ${(props) => (props.error ? theme.color.error : 'currentColor')};
+  font-size: ${theme.fontSize.default};
+  padding: 0;
+  min-height: 36px;
+  transition: border 0.2s ease-in-out;
+  width: ${(props) => (props.text ? '66%' : '100%')};
+  &:focus {
+    border-bottom: 1px solid ${theme.color.primary};
+  }
+`;
+
+const TextInput = ({
+  text = '',
+  id = '',
+  error = false,
+  fullWidth = false,
+  inputRef,
+  ...rest
+}: InputType) => {
+  return (
+    <StyledFormControl fullWidth={fullWidth}>
+      {text && <StyledLabel htmlFor={id}>{text}</StyledLabel>}
+      <StyledInput
+        {...rest}
+        ref={inputRef}
+        id={id}
+        text={text}
+        error={error}
+        fullWidth={fullWidth}
+      />
+    </StyledFormControl>
+  );
+};
+
+export default React.memo(TextInput);
