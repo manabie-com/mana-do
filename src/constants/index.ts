5ad31ae6c6b34c18890f7175dfc9d2b5d661036f
Nguyễn Minh Tuấn <tuan.nm@appfast.io>
2021-06-08 19:50:25 +0700

    Constants Added

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+// Notifications
+export const CONFIRM_CLEAR_ALL_TODOS =
+  "Are you sure you want to clear all todos?";
+export const CONFIRM_DELETE_TODO = "Are you sure you want to delete this todo?";
+export const TODO_PLACEHOLDER = "What need to be done?";
+export const CREATED_SUCCESS = "You just created a new todo!";
+export const CREATED_FAILED = "Please fill your todo's content!";
+export const UPDATED_SUCCESS = "This todo has been updated!";
+export const UPDATED_FAILED = "The content can't be empty!";
+export const DELETED_SUCCESS = "This todo has been deleted!";
+export const CLEARED_SUCCESS = "All todos have been cleared!";
+
+// Tooltips
+export const ALL_TOOLTIP = "Will show all todos when click!";
+export const ACTIVE_TOOLTIP = "Will show all active todos when click!";
+export const COMPLETED_TOOLTIP = "Will show all completed todos when click!";
+export const CLEAR_ALL_TOOLTIP = "Will clear all todos when click!";
