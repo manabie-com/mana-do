e209bdefcbc9e78bd039b2cb49946d844729f0a2
Nguyễn Minh Tuấn <tuan.nm@appfast.io>
2021-06-08 19:50:15 +0700

    Store Updated

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,64 +1,86 @@
-import {Todo, TodoStatus} from '../models/todo';
+import { Todo, TodoStatus } from "../models/todo";
 import {
   AppActions,
   CREATE_TODO,
   DELETE_ALL_TODOS,
   DELETE_TODO,
   TOGGLE_ALL_TODOS,
-  UPDATE_TODO_STATUS
-} from './actions';
+  UPDATE_TODO_STATUS,
+  UPDATE_TODO_CONTENT,
+} from "./actions";
 
 export interface AppState {
-  todos: Array<Todo>
+  todos: Array<Todo>;
 }
 
 export const initialState: AppState = {
-  todos: []
-}
+  todos: JSON.parse(localStorage.getItem("todoStorage") || "[]"),
+};
 
 function reducer(state: AppState, action: AppActions): AppState {
   switch (action.type) {
     case CREATE_TODO:
       state.todos.push(action.payload);
       return {
-        ...state
+        ...state,
       };
 
     case UPDATE_TODO_STATUS:
-      const index2 = state.todos.findIndex((todo) => todo.id === action.payload.todoId);
-      state.todos[index2].status = action.payload.checked ? TodoStatus.COMPLETED : TodoStatus.ACTIVE;
+      let statusIndex = state.todos.findIndex(
+        (todo) => todo.id === action.payload.todoId
+      );
+      // Condition for not existed id
+      if (statusIndex > -1)
+        state.todos[statusIndex].status = action.payload.checked
+          ? TodoStatus.COMPLETED
+          : TodoStatus.ACTIVE;
 
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: state.todos,
+      };
 
     case TOGGLE_ALL_TODOS:
-      const tempTodos = state.todos.map((e)=>{
+      const tempTodos = state.todos.map((e) => {
         return {
           ...e,
-          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE
-        }
-      })
+          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE,
+        };
+      });
 
       return {
         ...state,
-        todos: tempTodos
-      }
+        todos: tempTodos,
+      };
 
     case DELETE_TODO:
-      const index1 = state.todos.findIndex((todo) => todo.id === action.payload);
-      state.todos.splice(index1, 1);
-
+      // Changed from splice to filter for 2 reasons:
+      // 1. When the id is not exist, the index return -1,
+      // which will remove the last item from the todo list.
+      // 2. Shorter code
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
     case DELETE_ALL_TODOS:
       return {
         ...state,
-        todos: []
-      }
+        todos: [],
+      };
+
+    case UPDATE_TODO_CONTENT:
+      let contentIndex = state.todos.findIndex(
+        (todo) => todo.id === action.payload.todoId
+      );
+      if (contentIndex > -1)
+        state.todos[contentIndex].content = action.payload.value;
+
+      return {
+        ...state,
+        todos: state.todos,
+      };
+
     default:
       return state;
   }
