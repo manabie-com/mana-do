09d0565721b0f02c34850acf16b814e027f1ce18
Jimmy Phan <chiph@rnt.vn>
2021-06-14 16:03:02 +0700

    add input response

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -39,46 +39,48 @@ const SignInPage = () => {
 
     return (
         <div className="align-items-center wrap-box p-4">
-            <h1 className="align-items-center-title">Login</h1>
-            <p className="color-label">Sign In to your account</p>
-            <form onSubmit={signIn} className="m-t-15">
-                <label htmlFor="userId" className="align-items-center-group-input m-t-15">
-                    <div className="align-items-center-group-input-img">
-                        <img src={userIcon} className="align-items-center-group-input-icon" />
+            <div className="md-p-4">
+                <h1 className="align-items-center-title">Login</h1>
+                <p className="color-label">Sign In to your account</p>
+                <form onSubmit={signIn} className="m-t-15">
+                    <label htmlFor="userId" className="align-items-center-group-input m-t-15">
+                        <div className="align-items-center-group-input-img">
+                            <img src={userIcon} className="align-items-center-group-input-icon" />
+                        </div>
+                        <input
+                            id="user_id"
+                            name="userId"
+                            placeholder="Username"
+                            value={form.userId}
+                            className="input-none"
+                            onChange={onChangeField}
+                        />
+                    </label>
+                    <label htmlFor="password" className="align-items-center-group-input m-t-15">
+                        <div className="align-items-center-group-input-img">
+                            <img src={lockIcon} className="align-items-center-group-input-icon" />
+                        </div>
+                        <input
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            type="password"
+                            className="input-none"
+                            value={form.password}
+                            onChange={onChangeField}
+                        />
+                    </label>
+                    <br />
+                    {err && <div className="align-items-center-error m-t-15 md-p-4">
+                        <span>* Username or Password wrong!!!</span>
+                    </div>}
+                    <div className="align-items-center-button" >
+                        <button type="submit" className="btn-submit" style={{marginTop: 12}}>
+                            Sign in
+                        </button>
                     </div>
-                    <input
-                        id="user_id"
-                        name="userId"
-                        placeholder="Username"
-                        value={form.userId}
-                        className=" input-none"
-                        onChange={onChangeField}
-                    />
-                </label>
-                <label htmlFor="password" className="align-items-center-group-input m-t-15">
-                    <div className="align-items-center-group-input-img">
-                        <img src={lockIcon} className="align-items-center-group-input-icon" />
-                    </div>
-                    <input
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        type="password"
-                        className="input-none"
-                        value={form.password}
-                        onChange={onChangeField}
-                    />
-                </label>
-                <br />
-                {err && <div className="align-items-center-error m-t-15">
-                    <span>* Username or Password wrong!!!</span>
-                </div>}
-                <div className="align-items-center-button" >
-                    <button type="submit" className="btn-submit" style={{marginTop: 12}}>
-                        Sign in
-                    </button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     );
 };
