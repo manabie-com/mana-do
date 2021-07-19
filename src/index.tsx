bdade4b3a79d54eddade6708e2416bc03d551180
phuong.nguyen@websparks.sg <phuong.nguyen@websparks.sg>
2021-07-19 14:08:33 +0700

    assignment

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //remove strict mode to solve issue reducer dispatch twice
+    <App />,
   document.getElementById('root')
 );
 
