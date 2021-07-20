e132c9fb27f4be75726c69d97441a9425a6e6fc7
Hoang Minh Thai <thai.hoang@thai.hoang>
2021-07-20 23:33:38 +0700

    Add moduleNameMapper for unit test

--- jest.config.js
+++ jest.config.js
@@ -80,6 +80,9 @@ module.exports = {
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
