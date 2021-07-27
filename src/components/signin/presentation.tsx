947ff33726be83ee42236c56900c6dc4e1cecef8
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2021-07-28 06:53:27 +0700

    css for signin page

--- src/components/signin/presentation.tsx
+++ src/components/signin/presentation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './_signin.css'
 export interface ISignInForm {
   userId: string,
   password: string
@@ -21,35 +22,40 @@ const SignInPresentation = (props: SignInProps) => {
   const { signIn, form, onChangeField } = props
 
   return (
-    <div style={{ marginTop: '3rem', textAlign: 'left' }}>
-      <form onSubmit={signIn}>
-        <label htmlFor='user_id'>
-          {text.userId}
-          <input
-            id='user_id'
-            name='userId'
-            value={form.userId}
-            style={{ marginTop: 12 }}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor='password' >
-          {text.password}
-          <input
-            id='password'
-            name='password'
-            type='password'
-            style={{ marginTop: 12 }}
-            value={form.password}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <button type='submit' style={{ marginTop: 12 }}>
-          {text.signIn}
-        </button>
-      </form>
+    <div className='login-container'>
+      <div className='left-panel'></div>
+      <div className='right-panel'>
+        <form onSubmit={signIn}>
+          <label htmlFor='user_id'>
+            <h2>{text.userId}</h2>
+            <input
+              id='user_id'
+              name='userId'
+              value={form.userId}
+              style={{ marginTop: 12 }}
+              onChange={onChangeField}
+            />
+          </label>
+          <br />
+          <label htmlFor='password' >
+            <h2>{text.password}</h2>
+            <input
+              id='password'
+              name='password'
+              type='password'
+              style={{ marginTop: 12 }}
+              value={form.password}
+              onChange={onChangeField}
+            />
+          </label>
+          <br />
+          <div className='actions'>
+            <button type='submit'>
+              {text.signIn}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
