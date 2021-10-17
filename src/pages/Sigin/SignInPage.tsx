6b62b05501a9b9df4a0e9ae189d7bd6636ece877
Trung Nguyen <trung.nguyen@terralogic.com>
2021-10-17 16:10:38 +0700

    Custom login page

--- src/pages/Sigin/SignInPage.tsx
+++ src/pages/Sigin/SignInPage.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
 
 import {useHistory} from 'react-router-dom'
+import { ROUTE_TODO } from '../../routes';
 import Service from '../../service'
+
+import './style.css';
 
 const SignInPage = () => {
     const [form, setForm] = useState({
@@ -15,7 +18,7 @@ const SignInPage = () => {
         const resp = await Service.signIn(form.userId, form.password)
 
         localStorage.setItem('token', resp)
-        history.push('/todo')
+        history.push(ROUTE_TODO)
     }
 
     const onChangeField = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,36 +29,61 @@ const SignInPage = () => {
         }))
     }
 
+    const renderFormSignin = () => (
+        <form onSubmit={signIn} className='form-signin'>
+            <h1 className='form-signin-label'>Sign in</h1>
+            <label htmlFor='user_id' className='form-input'>
+                Username
+                <input
+                className='form-input__input'
+                placeholder='Username'
+                id='user_id'
+                name='userId'
+                value={form.userId}
+                onChange={onChangeField}
+                />
+            </label>
+            <br />
+            <label htmlFor='password' className='form-input'>
+                Password
+                <input
+                className='form-input__input'
+                placeholder='Password'
+                id='password'
+                name='password'
+                type='password'
+                value={form.password}
+                onChange={onChangeField}
+                />
+            </label>
+            <br />
+            <button type='submit' className='btn-signin'>
+                Sign in
+            </button>
+            <div className='form-action'>
+                <label className='remember'>
+                <input type='checkbox' />
+                <span>Remember</span>
+                </label>
+                <p className='forgot'>Forgot password</p>
+            </div>
+        </form>
+    );
+
+    const renderWelcome = () => (
+      <div className='welcome-section'>
+        <h1 className='welcome-label text'>Welcome to signin</h1>
+        <p className='sub-text text'>Don't have an account?</p>
+        <button className='btn-signup'>Sign up</button>
+      </div>
+    );
+
     return (
         <div style={{marginTop: '3rem', textAlign: 'left'}}>
-            <form onSubmit={signIn}>
-                <label htmlFor="user_id">
-                    User id
-                    <input
-                        id="user_id"
-                        name="userId"
-                        value={form.userId}
-                        style={{marginTop: 12}}
-                        onChange={onChangeField}
-                    />
-                </label>
-                <br/>
-                <label htmlFor="password" >
-                    Password
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        style={{marginTop: 12}}
-                        value={form.password}
-                        onChange={onChangeField}
-                    />
-                </label>
-                <br />
-                <button type="submit" style={{marginTop: 12}}>
-                    Sign in
-                </button>
-            </form>
+            <div className='signin'>
+                {renderFormSignin()}
+                {renderWelcome()}
+            </div>
         </div>
     );
 };
