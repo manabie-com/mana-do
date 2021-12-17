a1d1ec12f202adda5c6b3415490b93bd8a9f3d8d
Ha Truong <td.xuha@gmail.com>
2021-12-17 17:37:55 +0700

    fix: simplify reducer and add UPDATE_TODO case

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,64 +1,89 @@
-import {Todo, TodoStatus} from '../models/todo';
+import { Todo, TodoStatus } from "../models/todo";
 import {
   AppActions,
   CREATE_TODO,
+  UPDATE_TODO,
   DELETE_ALL_TODOS,
   DELETE_TODO,
   TOGGLE_ALL_TODOS,
-  UPDATE_TODO_STATUS
-} from './actions';
+  UPDATE_TODO_STATUS,
+} from "./actions";
 
 export interface AppState {
-  todos: Array<Todo>
+  todos: Array<Todo>;
 }
 
+// Use localStorage as initialState
 export const initialState: AppState = {
-  todos: []
-}
+  todos: JSON.parse(localStorage.getItem("localTodos") as string) || [],
+};
 
-function reducer(state: AppState, action: AppActions): AppState {
+function reducer(state: AppState, action: AppActions) {
   switch (action.type) {
     case CREATE_TODO:
-      state.todos.push(action.payload);
+      // Create a copy of the existing state and add values
       return {
-        ...state
+        ...state,
+        todos: [...state.todos, action.payload],
       };
 
-    case UPDATE_TODO_STATUS:
-      const index2 = state.todos.findIndex((todo) => todo.id === action.payload.todoId);
-      state.todos[index2].status = action.payload.checked ? TodoStatus.COMPLETED : TodoStatus.ACTIVE;
-
+    // Create UPDATE_TODO
+    case UPDATE_TODO:
+      const editTodo = state.todos.map((todo) => {
+        if (todo.id === action.payload.todoId)
+          return {
+            ...todo,
+            content: action.payload.newTodo,
+          };
+        return todo;
+      });
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: editTodo,
+      };
+
+    // Check if todo is checked.
+    case UPDATE_TODO_STATUS:
+      const updatedTodo = state.todos.map((todo) => {
+        if (todo.id === action.payload.todoId)
+          return {
+            ...todo,
+            status: action.payload.checked
+              ? TodoStatus.COMPLETED
+              : TodoStatus.ACTIVE,
+          };
+        return todo;
+      });
+      return {
+        ...state,
+        todos: updatedTodo,
+      };
 
     case TOGGLE_ALL_TODOS:
-      const tempTodos = state.todos.map((e)=>{
+      const tempTodos = state.todos.map((e) => {
         return {
           ...e,
-          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE
-        }
-      })
+          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE,
+        };
+      });
 
       return {
         ...state,
-        todos: tempTodos
-      }
+        todos: tempTodos,
+      };
 
+    // Delete todos that match IDs. Use filter instead of spice.
     case DELETE_TODO:
-      const index1 = state.todos.findIndex((todo) => todo.id === action.payload);
-      state.todos.splice(index1, 1);
-
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
     case DELETE_ALL_TODOS:
       return {
         ...state,
-        todos: []
-      }
+        todos: [],
+      };
     default:
       return state;
   }
