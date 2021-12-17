1152efecfbc3edb36ca25016e692bd26a6fff7a3
Ha Truong <td.xuha@gmail.com>
2021-12-17 17:39:02 +0700

    config(todos.ts): add typescriot interfaces

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,12 +1,27 @@
 export enum TodoStatus {
-  ACTIVE = 'ACTIVE',
-  COMPLETED = 'COMPLETED'
+  ACTIVE = "ACTIVE",
+  COMPLETED = "COMPLETED",
 }
 
 export interface Todo {
-  id: string
-  user_id: string
-  content : string
-  status?: TodoStatus
-  created_date: string
+  id: string;
+  user_id: string;
+  content: string;
+  status?: TodoStatus;
+  created_date: string;
+}
+
+export interface TodoEditInterface {
+  todo: Todo;
+  // setEdit: (type: boolean) => void;
+}
+
+export interface TodoListInterface {
+  todos: Todo[];
+  showing: TodoStatus | "ALL";
+}
+
+export interface TodoToolBarInterface {
+  todos: Todo[];
+  setShowing: (type: TodoStatus | "ALL") => void;
 }
