9c59ea324aaeccbed62792fe640210f7509f298d
Ha Truong <td.xuha@gmail.com>
2021-12-17 17:40:42 +0700

    feat(utils/ClickOutside.js): create ClickOutside func

---
+++ src/utils/ClickOutside.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export default function ClickOutside(ref, onClickOutside) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClickOutside();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, onClickOutside]);
+}
