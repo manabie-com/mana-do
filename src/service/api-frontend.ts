48e96d6de1043f353e4394c1f246c26311051951
LL <wgslancer@gmail.com>
2021-12-24 03:48:42 +0700

    Update: fix get todo list in localstorage error and refactor api-frontend code

--- src/service/api-frontend.ts
+++ src/service/api-frontend.ts
@@ -2,8 +2,15 @@ import { IAPI } from './types';
 import { Todo, TodoStatus } from '../models/todo';
 import shortid from 'shortid';
 
-const toDos = localStorage.getItem('todo-list');
 class ApiFrontend extends IAPI {
+  getTodoListInLocalStorage(): Array<Todo> {
+    const toDos = localStorage.getItem('todo-list');
+    if (!toDos) {
+      localStorage.setItem('todo-list', JSON.stringify([]));
+      return [];
+    }
+    return JSON.parse(toDos);
+  }
   async createTodo(content: string): Promise<Todo> {
     const newContent = {
       content,
@@ -12,25 +19,13 @@ class ApiFrontend extends IAPI {
       id: shortid(),
       user_id: 'firstUser',
     } as Todo;
-    if (!toDos) {
-      const newToDoList = [{ ...newContent }];
-      console.log(newToDoList);
-      localStorage.setItem('todo-list', JSON.stringify(newToDoList));
-    } else {
-      const newToDoList = JSON.parse(toDos);
-      localStorage.setItem(
-        'todo-list',
-        JSON.stringify([...newToDoList, newContent])
-      );
-    }
+    const toDos = this.getTodoListInLocalStorage();
+    localStorage.setItem('todo-list', JSON.stringify([...toDos, newContent]));
     return Promise.resolve(newContent);
   }
   async updateTodo(id: string, status: TodoStatus): Promise<Partial<Todo>> {
-    const toDos = localStorage.getItem('todo-list');
-    if (!toDos) {
-      return Promise.reject('Have an error when update Todo');
-    }
-    const newTodos = JSON.parse(toDos).map((todo: Todo) => {
+    const toDos = this.getTodoListInLocalStorage();
+    const newTodos = toDos.map((todo: Todo) => {
       return {
         ...todo,
         status: todo.id === id ? status : todo.status,
@@ -40,35 +35,28 @@ class ApiFrontend extends IAPI {
     return Promise.resolve({ id, status });
   }
   async deleteTodo(id: string): Promise<string> {
-    if (!toDos) {
-      return Promise.reject('Have an error when delete Todo');
-    }
+    const toDos = this.getTodoListInLocalStorage();
 
-    const indexToDelete = JSON.parse(toDos).findIndex(
-      (todo: Todo) => todo.id === id
-    );
+    const indexToDelete = toDos.findIndex((todo: Todo) => todo.id === id);
 
     if (indexToDelete === -1) {
       return 'Id not found';
     }
-    const newArray = JSON.parse(toDos).filter((todo: Todo) => todo.id !== id);
+    const newArray = toDos.filter((todo: Todo) => todo.id !== id);
     localStorage.setItem('todo-list', JSON.stringify(newArray));
     return 'Deleted';
   }
   async getTodos(): Promise<Todo[]> {
-    if (!toDos) {
-      localStorage.setItem('todo-list', JSON.stringify([]));
-      return [];
-    }
-    return JSON.parse(toDos);
+    const toDos = this.getTodoListInLocalStorage();
+    return toDos;
   }
   async deleteAllTodos(): Promise<string> {
     localStorage.setItem('todo-list', JSON.stringify([]));
     return Promise.resolve('Delete all completed');
   }
   async toggleAllTodos(checkAll: boolean): Promise<string> {
-    if (!toDos) return Promise.reject('Have an error when toggle all todos');
-    const newTodos = JSON.parse(toDos).map((todo: Todo): Todo => {
+    const toDos = this.getTodoListInLocalStorage();
+    const newTodos = toDos.map((todo: Todo): Todo => {
       return {
         ...todo,
         status: checkAll ? TodoStatus.COMPLETED : TodoStatus.ACTIVE,
@@ -78,8 +66,8 @@ class ApiFrontend extends IAPI {
     return Promise.resolve('Toggle all completed');
   }
   async updateContentTodo(id: string, content: string): Promise<string> {
-    if (!toDos) return Promise.reject('Have an error when update content todo');
-    const newToDoList = JSON.parse(toDos).map((todo: Todo): Todo => {
+    const toDos = this.getTodoListInLocalStorage();
+    const newToDoList = toDos.map((todo: Todo): Todo => {
       return {
         ...todo,
         content: id === todo.id ? content : todo.content,
