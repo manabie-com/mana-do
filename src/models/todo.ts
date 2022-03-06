91760758e6d3e7c7acf08ee6a068ce4f41e481d0
NTC <trongcong96@gmail.com>
2022-03-07 00:01:37 +0700

    Define an interface for the object model

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,8 +1,13 @@
 export enum TodoStatus {
-  ACTIVE = 'ACTIVE',
-  COMPLETED = 'COMPLETED'
+    ACTIVE = 'ACTIVE',
+    COMPLETED = 'COMPLETED'
 }
 
 export interface Todo {
-  [key: string]: any
+    //[key: string]: any,
+    id: string,
+    user_id: 'firstUser',
+    content: string,
+    status?: TodoStatus,
+    created_date: string,
 }
