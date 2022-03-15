1ff39a87a5b5b1438a3d723c62a176fd7fca0668
tql247 <tqlinh.exe@gmail.com>
2022-03-15 19:13:42 +0700

    use localstorage and format code

--- src/service/types.ts
+++ src/service/types.ts
@@ -1,6 +1,6 @@
-import {Todo} from '../models/todo';
+import { Todo } from "../models/todo";
 
 export abstract class IAPI {
-    abstract getTodos() : Promise<Array<Todo>>
-    abstract createTodo(content: string) : Promise<Todo>
+  abstract getTodos(): Promise<Array<Todo>>;
+  abstract createTodo(content: string): Promise<Todo>;
 }
