56bf1a0054e7e678ef5b2524c718487362344c28
tql247 <tqlinh.exe@gmail.com>
2022-03-15 19:11:46 +0700

    define todo models

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,8 +1,14 @@
 export enum TodoStatus {
+  ALL = 'ALL',
   ACTIVE = 'ACTIVE',
   COMPLETED = 'COMPLETED'
 }
 
 export interface Todo {
   [key: string]: any
+  id: string
+  user_id: string
+  content: string
+  status?: TodoStatus
+  created_date: string
 }
