dfcd88b8c8dc6e65751df6bab56effbc48b3e79f
tql247 <tqlinh.exe@gmail.com>
2022-03-16 14:05:03 +0700

    TodoInput and test

---
+++ src/components/TodoInput/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./index.css";
+
+export interface TodoInputProps {
+  inputRef: any;
+  _onCreateTodo(content: string): void;
+}
+
+export const TodoInput = (props: TodoInputProps) => {
+  const onCreateTodo = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // fire handler when user hit Enter
+    if (e.key === "Enter") {
+      // todo shouldn't empty
+      if (props.inputRef.current.value !== "") {
+        props._onCreateTodo(props.inputRef.current.value);
+      }
+    }
+  };
+
+  return (
+    <div className="Todo__creation bg-primary rounded p-5">
+      <div className="w-100 text-align-left">
+        <div className="font-size-large font-weight-bold color-white">
+          What need to be done?
+        </div>
+        <div className="color-white my-3">Type here then enter!</div>
+        <div className="w-100 d-flex">
+          <input
+            data-testid="todo-input"
+            ref={props.inputRef}
+            className="Todo__input"
+            onKeyDown={onCreateTodo}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
