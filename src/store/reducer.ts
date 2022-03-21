964db950db2ca8ca1ffc029e1c8b84d6daffe0ea
clark candia <clarkcandia@outlook.com>
2022-03-21 18:07:06 +0800

    Fixed and Added missing features

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,40 +1,50 @@
-import {Todo, TodoStatus} from '../models/todo';
+import { Todo, TodoStatus } from '../models/todo';
 import {
   AppActions,
   CREATE_TODO,
   DELETE_ALL_TODOS,
   DELETE_TODO,
   TOGGLE_ALL_TODOS,
+  UPDATE_TODO,
   UPDATE_TODO_STATUS
 } from './actions';
+
+// Local storage for todos
+const lsTodos: any = window.localStorage.getItem("todolist");
 
 export interface AppState {
   todos: Array<Todo>
 }
 
 export const initialState: AppState = {
-  todos: []
+  todos: JSON.parse(lsTodos) || []
 }
 
 function reducer(state: AppState, action: AppActions): AppState {
   switch (action.type) {
     case CREATE_TODO:
-      state.todos.push(action.payload);
+//  Fixed creat to do
       return {
-        ...state
+        ...state,
+        todos: [...state.todos, action.payload]
       };
+// Added feature UPDATE_TODO
+    case UPDATE_TODO:
+      const indexz = state.todos.findIndex((todo) => todo.id === action.payload.todoId);
+      state.todos[indexz].content = action.payload.content;
+      return {
+        ...state,
+      }
 
     case UPDATE_TODO_STATUS:
       const index2 = state.todos.findIndex((todo) => todo.id === action.payload.todoId);
       state.todos[index2].status = action.payload.checked ? TodoStatus.COMPLETED : TodoStatus.ACTIVE;
-
       return {
         ...state,
-        todos: state.todos
       }
 
     case TOGGLE_ALL_TODOS:
-      const tempTodos = state.todos.map((e)=>{
+      const tempTodos = state.todos.map((e) => {
         return {
           ...e,
           status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE
@@ -47,13 +57,15 @@ function reducer(state: AppState, action: AppActions): AppState {
       }
 
     case DELETE_TODO:
+      
       const index1 = state.todos.findIndex((todo) => todo.id === action.payload);
-      state.todos.splice(index1, 1);
-
+      state.todos.splice(index1,1);
       return {
         ...state,
         todos: state.todos
       }
+
+
     case DELETE_ALL_TODOS:
       return {
         ...state,
