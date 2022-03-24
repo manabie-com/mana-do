9c98786738b1a33311484638cfbe128c1e7897d8
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2022-03-24 13:20:42 +0700

    feat: Add another guide (just UI)

--- src/Guide.tsx
+++ src/Guide.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 
+type GuideItemProps={
+  text?:string
+}
+
+const GuideItem=({text}:GuideItemProps)=>{
+  return (
+    <div className="row mid">
+      <div className="Order"/>
+      <span>{text}</span>
+    </div>
+  )
+}
+
 const Guide=()=>{
   return (
     <div className="Guide">
       <span>Todo Guide!</span>
       <div>
-        <div className="row mid">
-          <div className="Order"/>
-          <span>Double click (todo-item) to edit!</span>
-        </div>
+        <GuideItem text="Double click (todo-item) to edit."/>
+        <GuideItem text="You can scroll to show more todo-item."/>
       </div>
     </div>
   )
