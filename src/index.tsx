414a2ece918044c74cc4e62ffca6973568f84cde
tungphan311 <thanhtunga1lqd@gmail.com>
2022-04-06 14:16:37 +0700

    [fix]: prevent app create 2 to-do at a time

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Updated: I removed React Strict Mode, because Strict Mode sometimes tests reducer
+// automatically by calling it twice. So that, it cause createTodo action sometimes
+// create 2 to-do in list
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
