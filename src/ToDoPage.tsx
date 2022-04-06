c814f348d969253343bcbf606e0094451c80dfd1
tungphan311 <thanhtunga1lqd@gmail.com>
2022-04-06 14:31:27 +0700

    [fix]: handle onChange of checkbox

--- src/ToDoPage.tsx
+++ src/ToDoPage.tsx
@@ -1,107 +1,110 @@
-import React, {useEffect, useReducer, useRef, useState} from 'react';
+import React, { useEffect, useReducer, useRef, useState } from "react";
 
-import reducer, {initialState} from './store/reducer';
+import reducer, { initialState } from "./store/reducer";
 import {
-    setTodos,
-    createTodo,
-    toggleAllTodos,
-    deleteAllTodos,
-    updateTodoStatus
-} from './store/actions';
-import Service from './service';
-import {TodoStatus} from './models/todo';
+  setTodos,
+  createTodo,
+  toggleAllTodos,
+  deleteAllTodos,
+  updateTodoStatus,
+} from "./store/actions";
+import Service from "./service";
+import { Todo, TodoStatus } from "./models/todo";
 
-type EnhanceTodoStatus = TodoStatus | 'ALL';
-
+type EnhanceTodoStatus = TodoStatus | "ALL";
 
 const ToDoPage = () => {
-    const [{todos}, dispatch] = useReducer(reducer, initialState);
-    const [showing, setShowing] = useState<EnhanceTodoStatus>('ALL');
-    const inputRef = useRef<any>(null);
+  const [{ todos }, dispatch] = useReducer(reducer, initialState);
+  const [showing, setShowing] = useState<EnhanceTodoStatus>("ALL");
+  const inputRef = useRef<any>(null);
 
-    useEffect(()=>{
-        (async ()=>{
-            const resp = await Service.getTodos();
+  useEffect(() => {
+    (async () => {
+      const resp = await Service.getTodos();
 
-            dispatch(setTodos(resp || []));
-        })()
-    }, [])
+      dispatch(setTodos(resp || []));
+    })();
+  }, []);
 
-    const onCreateTodo = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter' ) {
-            const resp = await Service.createTodo(inputRef.current.value);
-            dispatch(createTodo(resp));
-        }
+  const onCreateTodo = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const resp = await Service.createTodo(inputRef.current.value);
+      dispatch(createTodo(resp));
     }
+  };
 
-    const onUpdateTodoStatus = (e: React.ChangeEvent<HTMLInputElement>, todoId: any) => {
-        dispatch(updateTodoStatus(todoId, e.target.checked))
-    }
+  const onUpdateTodoStatus = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    todoId: string
+  ) => {
+    dispatch(updateTodoStatus(todoId, e.target.checked));
+  };
 
-    const onToggleAllTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(toggleAllTodos(e.target.checked))
-    }
+  const onToggleAllTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(toggleAllTodos(e.target.checked));
+  };
 
-    const onDeleteAllTodo = () => {
-        dispatch(deleteAllTodos());
-    }
+  const onDeleteAllTodo = () => {
+    dispatch(deleteAllTodos());
+  };
 
-
-    return (
-        <div className="ToDo__container">
-            <div className="Todo__creation">
-                <input
-                    ref={inputRef}
-                    className="Todo__input"
-                    placeholder="What need to be done?"
-                    onKeyDown={onCreateTodo}
-                />
+  return (
+    <div className="ToDo__container">
+      <div className="Todo__creation">
+        <input
+          ref={inputRef}
+          className="Todo__input"
+          placeholder="What need to be done?"
+          onKeyDown={onCreateTodo}
+        />
+      </div>
+      <div className="ToDo__list">
+        {todos.map(({ id, status, content }: Todo) => {
+          return (
+            /** Fixed:
+             * 1. I change key from index to todo.id because it's not recommend to use index of item to store as key.
+             * 2. Bug: on calling onUpdateTodoStatus, it's a mistake calling index as todoId
+             * => I change it into id of item
+             */
+            <div key={id} className="ToDo__item">
+              <input
+                type="checkbox"
+                checked={showing === status}
+                onChange={(e) => onUpdateTodoStatus(e, id)}
+              />
+              <span>{content}</span>
+              <button className="Todo__delete">X</button>
             </div>
-            <div className="ToDo__list">
-                {
-                    todos.map((todo, index) => {
-                        return (
-                            <div key={index} className="ToDo__item">
-                                <input
-                                    type="checkbox"
-                                    checked={showing === todo.status}
-                                    onChange={(e) => onUpdateTodoStatus(e, index)}
-                                />
-                                <span>{todo.content}</span>
-                                <button
-                                    className="Todo__delete"
-                                >
-                                    X
-                                </button>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-            <div className="Todo__toolbar">
-                {todos.length > 0 ?
-                    <input
-                        type="checkbox"
-                        onChange={onToggleAllTodo}
-                    /> : <div/>
-                }
-                <div className="Todo__tabs">
-                    <button className="Action__btn">
-                        All
-                    </button>
-                    <button className="Action__btn" onClick={()=>setShowing(TodoStatus.ACTIVE)}>
-                        Active
-                    </button>
-                    <button className="Action__btn" onClick={()=>setShowing(TodoStatus.COMPLETED)}>
-                        Completed
-                    </button>
-                </div>
-                <button className="Action__btn" onClick={onDeleteAllTodo}>
-                    Clear all todos
-                </button>
-            </div>
+          );
+        })}
+      </div>
+      <div className="Todo__toolbar">
+        {todos.length > 0 ? (
+          <input type="checkbox" onChange={onToggleAllTodo} />
+        ) : (
+          <div />
+        )}
+        <div className="Todo__tabs">
+          <button className="Action__btn">All</button>
+          <button
+            className="Action__btn"
+            onClick={() => setShowing(TodoStatus.ACTIVE)}
+          >
+            Active
+          </button>
+          <button
+            className="Action__btn"
+            onClick={() => setShowing(TodoStatus.COMPLETED)}
+          >
+            Completed
+          </button>
         </div>
-    );
+        <button className="Action__btn" onClick={onDeleteAllTodo}>
+          Clear all todos
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ToDoPage;
--- src/service/api-frontend.ts
+++ src/service/api-frontend.ts
@@ -3,27 +3,27 @@ import { Todo, TodoStatus } from "../models/todo";
 import shortid from "shortid";
 
 class ApiFrontend extends IAPI {
-    async createTodo(content: string): Promise<Todo> {
-        return Promise.resolve({
-            content: content,
-            created_date: new Date().toISOString(),
-            status: TodoStatus.ACTIVE,
-            id: shortid(),
-            user_id: "firstUser",
-        } as Todo);
-    }
+  async createTodo(content: string): Promise<Todo> {
+    return Promise.resolve({
+      content: content,
+      created_date: new Date().toISOString(),
+      status: TodoStatus.ACTIVE,
+      id: shortid(),
+      user_id: "firstUser",
+    } as Todo);
+  }
 
-    async getTodos(): Promise<Todo[]> {
-        return [
-            {
-                content: "Content",
-                created_date: new Date().toISOString(),
-                status: TodoStatus.ACTIVE,
-                id: shortid(),
-                user_id: "firstUser",
-            } as Todo,
-        ];
-    }
+  async getTodos(): Promise<Todo[]> {
+    return [
+      {
+        content: "Content",
+        created_date: new Date().toISOString(),
+        status: TodoStatus.ACTIVE,
+        id: shortid(),
+        user_id: "firstUser",
+      } as Todo,
+    ];
+  }
 }
 
 export default new ApiFrontend();
