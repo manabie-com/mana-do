2761a2207b0328e20bbbb18b7cbe502c5c57e195
Hien <ddhien.cmc@gmail.com>
2022-04-19 12:26:55 +0700

    Add edit todo and save to localstorage

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,64 +1,104 @@
-import {Todo, TodoStatus} from '../models/todo';
+import { Todo, TodoStatus } from "../models/todo";
 import {
   AppActions,
   CREATE_TODO,
   DELETE_ALL_TODOS,
   DELETE_TODO,
+  EDIT_TODO,
+  SET_TODO,
   TOGGLE_ALL_TODOS,
-  UPDATE_TODO_STATUS
-} from './actions';
+  UPDATE_TODO_STATUS,
+} from "./actions";
 
 export interface AppState {
-  todos: Array<Todo>
+  todos: Array<Todo>;
 }
 
 export const initialState: AppState = {
-  todos: []
-}
+  todos: [],
+};
 
 function reducer(state: AppState, action: AppActions): AppState {
   switch (action.type) {
-    case CREATE_TODO:
-      state.todos.push(action.payload);
+    //
+    case SET_TODO:
       return {
-        ...state
+        ...state,
+        todos: action.payload,
+      };
+
+    case CREATE_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+
+    case EDIT_TODO:
+      const index3 = state.todos.findIndex(
+        (todo) => todo.id === action.payload.todoId
+      );
+      return {
+        ...state,
+        todos: state.todos.map((todo, index) => {
+          if (index === index3) {
+            return {
+              ...todo,
+              content: action.payload.content,
+            };
+          }
+          return todo;
+        }),
       };
 
     case UPDATE_TODO_STATUS:
-      const index2 = state.todos.findIndex((todo) => todo.id === action.payload.todoId);
-      state.todos[index2].status = action.payload.checked ? TodoStatus.COMPLETED : TodoStatus.ACTIVE;
-
+      const index2 = state.todos.findIndex(
+        (todo) => todo.id === action.payload.todoId
+      );
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: state.todos.map((todo, index) => {
+          if (index === index2) {
+            return {
+              ...todo,
+              status: action.payload.checked
+                ? TodoStatus.COMPLETED
+                : TodoStatus.ACTIVE,
+            };
+          }
+          return todo;
+        }),
+      };
 
     case TOGGLE_ALL_TODOS:
-      const tempTodos = state.todos.map((e)=>{
+      const tempTodos = state.todos.map((e) => {
         return {
           ...e,
-          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE
-        }
-      })
+          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE,
+        };
+      });
 
       return {
         ...state,
-        todos: tempTodos
-      }
+        todos: tempTodos,
+      };
 
     case DELETE_TODO:
-      const index1 = state.todos.findIndex((todo) => todo.id === action.payload);
-      state.todos.splice(index1, 1);
+      //
+      const index1 = state.todos.findIndex(
+        (todo) => todo.id === action.payload
+      );
+      const todos1 = state.todos.slice();
+      todos1.splice(index1, 1);
 
       return {
         ...state,
-        todos: state.todos
-      }
+        todos: todos1,
+      };
     case DELETE_ALL_TODOS:
       return {
         ...state,
-        todos: []
-      }
+        todos: [],
+      };
     default:
       return state;
   }
