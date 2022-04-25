9ca765b4f8e73f22ff150caf9f7d849807de1baa
“ariane-fullstack” <ariane@fullstack.ph>
2022-04-25 09:50:48 +0800

    feat: add header, change head title, update styling

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='Nav'>
+      <div className='Nav__container'>
+        <span className='Nav__image'>✔️</span>
+        <h1 className='Nav__text'>Mana-do</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
