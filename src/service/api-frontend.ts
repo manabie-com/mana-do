678ab4b5ec07f55739c9306780bffacec74b5032
Trung Ngo <trung.ngo@kyanon.digital>
2022-05-08 16:24:24 +0700

    ref:  use absolute path instead of relative path

--- src/service/api-frontend.ts
+++ src/service/api-frontend.ts
@@ -1,29 +1,29 @@
-import { IAPI } from "./types";
-import { Todo, TodoStatus } from "../models/todo";
 import shortid from "shortid";
+import { Todo, TodoStatus } from "models/todo";
+import { IAPI } from "./types";
 
 class ApiFrontend extends IAPI {
-    async createTodo(content: string): Promise<Todo> {
-        return Promise.resolve({
-            content: content,
-            created_date: new Date().toISOString(),
-            status: TodoStatus.ACTIVE,
-            id: shortid(),
-            user_id: "firstUser",
-        } as Todo);
-    }
+	async createTodo(content: string): Promise<Todo> {
+		return Promise.resolve({
+			content: content,
+			created_date: new Date().toISOString(),
+			status: TodoStatus.ACTIVE,
+			id: shortid(),
+			user_id: "firstUser",
+		} as Todo);
+	}
 
-    async getTodos(): Promise<Todo[]> {
-        return [
-            {
-                content: "Content",
-                created_date: new Date().toISOString(),
-                status: TodoStatus.ACTIVE,
-                id: shortid(),
-                user_id: "firstUser",
-            } as Todo,
-        ];
-    }
+	async getTodos(): Promise<Todo[]> {
+		return [
+			{
+				content: "Content",
+				created_date: new Date().toISOString(),
+				status: TodoStatus.ACTIVE,
+				id: shortid(),
+				user_id: "firstUser",
+			} as Todo,
+		];
+	}
 }
 
 export default new ApiFrontend();
