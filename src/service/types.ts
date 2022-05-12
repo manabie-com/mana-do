fbf555a93ca4738c807bc222577787fb19f9e429
binh.phan <phanthanhbinh296@gmail.com>
2022-05-12 17:37:55 +0700

    phan thanh binh frontend submit

--- src/service/types.ts
+++ src/service/types.ts
@@ -1,6 +1,9 @@
-import {Todo} from '../models/todo';
+import { Todo } from '../models/todo';
 
 export abstract class IAPI {
-    abstract getTodos() : Promise<Array<Todo>>
-    abstract createTodo(content: string) : Promise<Todo>
+    abstract getTodos(): Promise<Array<Todo>>
+    abstract createTodo(content: string): Promise<Todo>
+    abstract updateTodo(todo: Todo, id: string): Promise<Todo>
+    abstract deleteTodo(id: string): Promise<string>
+    abstract saveTodos(todos: Todo[]): Promise<Array<Todo>>
 }
