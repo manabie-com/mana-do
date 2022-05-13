44d644369de4b784290c0e1180368fb816f7bb09
Trung Ngo <trung.ngo@kyanon.digital>
2022-05-13 09:13:13 +0700

    feat: add feature delete multi todos, filter by status

--- src/ToDoPage.tsx
+++ src/ToDoPage.tsx
@@ -1,24 +1,45 @@
-import React, { useEffect, useReducer, useState } from "react";
-
+import React, { useEffect, useReducer, useState, useMemo } from "react";
+import cs from "classnames";
 import reducer, { initialState } from "store/reducer";
 import {
 	setTodos,
 	createTodo,
 	toggleAllTodos,
-	deleteAllTodos,
 	updateTodoStatus,
 	deleteTodos,
+	toggleTickTodoAction,
+	toggleTickALLTodosAction,
 } from "store/actions";
 import Service from "service";
 import { TodoStatus } from "models/todo";
 import TodoItem from "components/TodoItem";
 import TodoForm from "components/TodoForm";
 
+const ALL_ITEM = "ALL";
 type EnhanceTodoStatus = TodoStatus | "ALL";
 
 const ToDoPage = () => {
-	const [{ todos }, dispatch] = useReducer(reducer, initialState);
+	const [{ todos, selectedItemIds }, dispatch] = useReducer(
+		reducer,
+		initialState,
+	);
 	const [showing, setShowing] = useState<EnhanceTodoStatus>("ALL");
+
+	const itemsAfterFilter = useMemo(() => {
+		if (showing === ALL_ITEM) {
+			return todos;
+		}
+		return todos.filter((item) => item.status === showing);
+	}, [showing, todos]);
+
+	const itemsSelectedAfterFilter = useMemo(() => {
+		return itemsAfterFilter.filter((item) => {
+			return selectedItemIds.indexOf(item.id) >= 0;
+		});
+	}, [itemsAfterFilter, selectedItemIds]);
+
+	const tickAllTodos =
+		itemsSelectedAfterFilter.length === itemsAfterFilter.length;
 
 	useEffect(() => {
 		(async () => {
@@ -40,8 +61,20 @@ const ToDoPage = () => {
 		dispatch(toggleAllTodos(e.target.checked));
 	};
 
-	const onDeleteAllTodo = () => {
-		dispatch(deleteAllTodos());
+	// remove items are selected. If itemsSelectedAfterFilter is empty We will delete all items are filterd by filter menu
+	const onDeleteAllTodo = async () => {
+		let ids = [];
+
+		if (itemsSelectedAfterFilter.length) {
+			ids = itemsSelectedAfterFilter.map((i) => i.id);
+		} else {
+			ids = itemsAfterFilter.map((i) => i.id);
+		}
+
+		const result = await Service.batchDelete(ids);
+		if (result) {
+			dispatch(deleteTodos(ids));
+		}
 	};
 
 	const onDeleteTodo = async (id: string) => {
@@ -51,45 +84,79 @@ const ToDoPage = () => {
 		}
 	};
 
+	const onTickTodo = (id: string, checked: boolean) => {
+		dispatch(toggleTickTodoAction(id, checked));
+	};
+
+	const onTickAllTodos = (e: React.ChangeEvent<HTMLInputElement>) => {
+		dispatch(toggleTickALLTodosAction(e.target.checked));
+	};
+
 	return (
 		<div className="ToDo__container">
 			<div className="Todo__creation">
 				<TodoForm onSubmit={handSubmit} />
 			</div>
 			<div className="ToDo__list">
-				{todos.map((todo, index) => {
+				{itemsAfterFilter.map((todo) => {
 					return (
 						<TodoItem
 							{...todo}
 							key={todo.id}
-							active={showing === todo.status}
+							active={selectedItemIds.indexOf(todo.id) >= 0}
 							onChangeStatus={onUpdateTodoStatus}
 							onRemove={onDeleteTodo}
+							onTick={onTickTodo}
 						/>
 					);
 				})}
 			</div>
 			<div className="Todo__toolbar">
-				{todos.length > 0 ? (
-					<input type="checkbox" onChange={onToggleAllTodo} />
+				{itemsAfterFilter.length > 0 ? (
+					<input
+						aria-label="todo-page-check-all"
+						type="checkbox"
+						checked={tickAllTodos}
+						onChange={onTickAllTodos}
+					/>
 				) : (
 					<div />
 				)}
 				<div className="Todo__tabs">
-					<button className="Action__btn">All</button>
 					<button
-						className="Action__btn"
+						aria-label="todo-page-filter-all"
+						className={cs("Action__btn", {
+							"Action__btn--primary": showing === ALL_ITEM,
+						})}
+						onClick={() => setShowing(ALL_ITEM)}>
+						All
+					</button>
+					<button
+						aria-label="todo-page-filter-active"
+						className={cs("Action__btn", {
+							"Action__btn--primary":
+								showing === TodoStatus.ACTIVE,
+						})}
 						onClick={() => setShowing(TodoStatus.ACTIVE)}>
 						Active
 					</button>
 					<button
-						className="Action__btn"
+						aria-label="todo-page-filter-completed"
+						className={cs("Action__btn", {
+							"Action__btn--primary":
+								showing === TodoStatus.COMPLETED,
+						})}
 						onClick={() => setShowing(TodoStatus.COMPLETED)}>
 						Completed
 					</button>
 				</div>
-				<button className="Action__btn" onClick={onDeleteAllTodo}>
-					Clear all todos
+				<button
+					aria-label="todo-page-delete"
+					className="Action__btn Todo__toolbar__delete-all"
+					onClick={onDeleteAllTodo}>
+					{tickAllTodos || !itemsSelectedAfterFilter.length
+						? "Clear all todos"
+						: `Clear ${itemsSelectedAfterFilter.length} todo(s) selected`}
 				</button>
 			</div>
 		</div>
