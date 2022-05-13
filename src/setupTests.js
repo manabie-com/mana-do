9d1340c029ba6960621628a00c02af05684d0031
Trung Ngo <trung.ngo@kyanon.digital>
2022-05-13 08:46:37 +0700

    feat: add config for testing

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+
+const localStorageMock = {
+	getItem: jest.fn(),
+	setItem: jest.fn(),
+	removeItem: jest.fn(),
+	clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
