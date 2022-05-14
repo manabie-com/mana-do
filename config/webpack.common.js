260e10a9f647d84d5849f7f47e046b7dfdfd02a8
phuoc <phuoc.leo@bsgglobal.co.kr>
2022-05-14 16:40:26 +0700

    chore: setup eslint prettier

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const paths = require('./paths');
 const CopyWebpack = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -49,17 +49,8 @@ module.exports = {
     }),
 
     // compresses assets
-    new BrotliGzipPlugin({
+    new BrotliPlugin({
       asset: '[path].br[query]',
-      algorithm: 'brotli',
-      test: /\.(js|css|html|svg)$/,
-      threshold: 10240,
-      minRatio: 0.8,
-      quality: 11,
-    }),
-    new BrotliGzipPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
       test: /\.(js|css|html|svg)$/,
       threshold: 10240,
       minRatio: 0.8,
